d15f28d1db786050caed6f4624f7ced09f1244a8
octa230 <mervynstunner@outlook.com>
2022-07-25 17:04:57 +0300

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data ={
+    products: [
+        {
+            name: 'stylish spring',
+            slug: 'stylish-spring',
+            category: 'bouquet',
+            image:'/images/p1.jpg', //679px X 829px
+            price: 120,
+            countInStock: 10,
+            rating: 4.5,
+            Reviews: 10,
+            description: 'best quality'
+        },
+        {
+            name: 'Mellow Yellow',
+            slug: 'Mellow-yellow',
+            category: 'bouquet',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            rating: 4.5,
+            Reviews: 10,
+            description: 'best quality',
+        },
+        {
+            name: 'Red Rose',
+            slug: 'Red-rose',
+            category: 'bouquet',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            rating: 4.5,
+            Reviews: 10,
+            description: 'best quality'
+        },
+        {
+            name: 'My Beautiful Pastel',
+            slug: 'My Beautiful Pastel',
+            category: 'box',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            rating: 4.5,
+            Reviews: 10,
+            description: 'best quality'
+        },
+        {
+            name: 'Mellow Box',
+            slug: 'Mellow-Box',
+            category: 'box',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            rating: 4.5,
+            Reviews: 10,
+            description: 'best quality'
+        },
+        {
+            name: 'Glory box',
+            slug: 'Glory-box',
+            category: 'box',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            rating: 4.5,
+            Reviews: 10,
+            description: 'best quality'
+        },
+    ],
+}
+export default data
